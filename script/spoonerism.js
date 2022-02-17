ff81a47757549181345c8e5fcd74220b8aeeca12
bx-NT <bx-nt@bx.kmutt.ac.th>
2022-02-17 18:20:48 +0700

    add loo-lang api

--- script/spoonerism.js
+++ script/spoonerism.js
@@ -1,3 +1,20 @@
-const checkSwap = (syllable) =>{
-    return syllable+'aa'
+const tokenizer = (syllable) =>{
+    $.ajax({
+        url: "https://loo-lang.herokuapp.com/spoon",
+        type: "post",
+        data: {
+            data: syllable
+        },
+        dataType: 'json',
+        success: (data, status) => {
+            console.log(data, status);
+            $("#outp").text(data);
+        }
+    });
+    return syllable
+}
+const checkSwap = (syl) => {
+    if(length(syl) === 2){
+        syl[0], syl[1] = syl[1], syl[0]
+    }
 }
